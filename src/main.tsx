47edfac7308dfb370fc5a84a88680b7e25522c8f
EunJiJung <bianbbc87@gmail.com>
2025-03-29 16:36:13 +0900

    setting: coreui 기본 admin page tsx로 변경
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
-import './index.css'
-import App from './App.tsx'
+import { Provider } from 'react-redux'
+import 'core-js'
+
+import App from './App'
+import store from './store'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
 )
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import AppSidebar from '@components/AppSidebar'
+import AppHeader from '@components/AppHeader'
+import AppFooter from '@components/AppFooter'
+import AppContent from '@components/AppContent'
+import '@coreui/coreui/dist/css/coreui.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <div>
+    <AppSidebar />
+    <div className="wrapper d-flex flex-column min-vh-100">
+      <AppHeader />
+      <div className="body flex-grow-1">
+        <AppContent />
+      </div>
+      <AppFooter />
+    </div>
+  </div>
   )
 }
